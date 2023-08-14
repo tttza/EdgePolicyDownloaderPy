36534a95d457a26a8b7e5e01122917200d826607
tttza <25410554+tttza@users.noreply.github.com>
2023-08-14 18:06:14 +0900

    Add deploy policy feature.

---
+++ main.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import shutil
+import ctypes
+import pandas as pd
+
+import DownloadPolicy as DP
+
+
+def display_versions_and_choose(df):
+    df = df.copy().sort_values('Channel', ascending=False)\
+        .sort_values('Version', ascending=False).reset_index(drop=True)
+    # 表の表示
+    print(df[['Version', 'Channel']])
+    print()
+
+    # ユーザーに選択させる
+    indexes = list(df.index)
+    versions = list(df['Version'])
+    print("キャンセルするには 'c' と入力してください。")
+    chosen_input = ""
+    while True:
+        chosen_input = input("インデックス または バージョン文字列 を入力してください（Default: 最新Stable）：")
+        chosen_input = chosen_input.strip()
+        if chosen_input == 'c':
+            return None
+        if not chosen_input:  # デフォルトの選択（最新のStable版）
+            latest_stable_version = df[df['Channel'] == 'stable']['Version'].max()
+            return latest_stable_version
+        if chosen_input.isdigit() and int(chosen_input) in indexes:
+            return df.loc[int(chosen_input), 'Version']
+        elif chosen_input in versions:
+            return chosen_input
+
+def is_admin():
+    try:
+        # 管理者権限で実行されているかを確認するために、os.getuid() を使用
+        # Windowsでは、os.getuid()は存在しないので、AttributeErrorが発生
+        return os.getuid() == 0
+    except AttributeError:
+        return os.system("net session >nul 2>&1") == 0
+
+
+def copy_files_to_policy_dir(src_path=r"policy/windows/admx"):
+    # dest_path = "C:\\Windows\\PolicyDefinitions"
+    dest_path = "C:\\temp\\PolicyDefinitions"
+
+    for item in os.listdir(src_path):
+        src_item = os.path.join(src_path, item)
+        dest_item = os.path.join(dest_path, item)
+
+        # ファイルの場合
+        if os.path.isfile(src_item):
+            shutil.copy2(src_item, dest_item)
+            print(f"{item} を {dest_path} にコピーしました。")
+        # ディレクトリの場合
+        elif os.path.isdir(src_item):
+            if not os.path.exists(dest_item):  # ディレクトリが存在しない場合、新しく作成
+                os.makedirs(dest_item)
+            for sub_item in os.listdir(src_item):
+                src_sub_item = os.path.join(src_item, sub_item)
+                dest_sub_item = os.path.join(dest_item, sub_item)
+                if os.path.isfile(src_sub_item):
+                    shutil.copy2(src_sub_item, dest_sub_item)
+            print(f"{item} ディレクトリの内容を {dest_path} にコピーしました。")
+
+
+if __name__ == "__main__":
+    try:
+        # 管理者権限の確認
+        if not is_admin():
+            print("この操作を行うには管理者権限が必要です。")
+
+            # # UACプロンプトを表示して管理者権限でスクリプトを再実行
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+            exit(0)
+
+        df = DP.get_versions()
+
+        # ユーザーがバージョンを選択
+        selected_version = display_versions_and_choose(df)
+        print()
+        print("Version:", selected_version)
+
+        if not selected_version:
+            print("操作がキャンセルされました。")
+            input("Enterキーを押して終了してください。")
+            exit(0)
+
+        print("ダウンロード中...\n")
+        cab_filename = DP.download_version(selected_version, df)
+        zip_filename = DP.extract_cab(cab_filename)
+        if zip_filename:
+            DP.extract_zip(zip_filename)
+
+            # cab と zip ファイルを削除
+            os.remove(cab_filename)
+            os.remove(zip_filename)
+
+            # ファイルを C:\windows\PolicyDefinitions にコピー
+            copy_files_to_policy_dir()
+
+        print()
+        input("Enterキーを押して終了してください。")
+
+    except Exception as e:
+        print()
+        print(e)
+        input("Enterキーを押して終了してください。")
